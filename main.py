38c546a55aa64d50c23522f42dbb4fd4db5f8819
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-03-23 17:41:09 +0500

    add hi_page

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return "Привет от приложения Flask"
 
+@app.route("/hi")
+def index():
+    return "HI"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
